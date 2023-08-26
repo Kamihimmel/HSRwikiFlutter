74420ee329d22c2fddb68ffbc8237d90395132be
Kamihimmel <yunlu18@gmail.com>
2023-08-27 01:22:47 +0900

    add basic task

--- lib/components/basic_panel.dart
+++ lib/components/basic_panel.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 import '../characters/character.dart';
 import '../characters/character_manager.dart';
@@ -16,17 +19,184 @@ class BasicPanelState extends State<BasicPanel> {
     super.initState();
   }
 
+  //ANCHOR method:statbuilder
+  Row buildStatRow(Color color, String a, String b) {
+    return Row(
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          color: color,
+        ),
+        Column(
+          children: [
+            SelectableText(
+              a,
+              style: TextStyle(
+                fontSize: 10,
+                height: 1.1,
+              ),
+            ),
+            SelectableText(
+              b,
+              style: TextStyle(
+                fontSize: 10,
+                height: 1.1,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  //ANCHOR method:statbarbuilder:percent
+  Column buildstatbarpercent(Color color, var a) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 500),
+          width: a * 2 + 0.0,
+          height: 20,
+          color: color,
+        ),
+      ],
+    );
+  }
+
+  //ANCHOR method:statbarbuilder
+  Column buildstatbar(Color color, var a) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 500),
+          width: a / 7 * _gs.statScale / 10,
+          height: 20,
+          color: color,
+        ),
+      ],
+    );
+  }
+
+  //ANCHOR method:statbarbuilder(HP)
+  Column buildstatbarhp(Color color, var a) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 500),
+          width: a / 75 * _gs.statScale / 10,
+          height: 20,
+          color: color,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Character _cData = CharacterManager.getCharacter(_gs.stats.id);
     return ChangeNotifierProvider.value(
         value: _gs.getCharacterStats(),
         child: Consumer<CharacterStatsNotify>(
-            builder: (context, model, child) => Container()));
+            builder: (context, model, child) => Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(15)),
+                          border: Border.all(color: Colors.white.withOpacity(0.13)),
+                          gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [_cData.elementType.color.withOpacity(0.35), Colors.black.withOpacity(0.5)]),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: SelectableText(
+                                'Basic Panel'.tr(),
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            //ANCHOR ATK
+                            SelectableText(
+                              '${"ATK".tr()}:500 + ' + "300" + ' = ' + "800",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child:
+                                  //level:Colors.red
+                                  //weapon:Colors.blue
+                                  //weapon%:Colors.green
+                                  //a1:Colors.yellow
+                                  //a1%:Colors.yellow[700]
+                                  //a2:Colors.pink
+                                  //a2%:Colors.pink[700]
+                                  //a3%:Colors.blueGrey
+                                  //a3:Colors.blueGrey[700]
+                                  //a4%:Colors.purple
+                                  //a4:Colors.purple[700]
+                                  //a5%:Colors.teal
+                                  //a5:Colors.teal[700]
+                                  //pyro2On:Colors.red
+                                  //gladiator2On:Colors.purple
+                                  //royalflora4On:Colors.purple
+
+                                  FractionallySizedBox(
+                                widthFactor: 1.0,
+                                child: Scrollbar(
+                                  child: SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        //ANCHOR statATK:stats
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            buildStatRow(Colors.red, 'level'.tr(), '250'),
+                                            buildStatRow(Colors.blue, 'weapon'.tr(), '250'),
+                                            buildStatRow(Colors.red, '${"level".tr()}%(60)', double.parse((0.6 * 250 / 100).toStringAsFixed(1)).toString()),
+                                            buildStatRow(Colors.green, '${"weapon".tr()}%(60)', double.parse((0.6 * 250 / 100).toStringAsFixed(1)).toString()),
+                                          ],
+                                        ),
+                                        //ANCHOR statATK:bar
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            buildstatbar(Colors.red, 250),
+                                            buildstatbar(Colors.blue, 250),
+                                            buildstatbar(Colors.red, (0.6 * 250 / 100)),
+                                            buildstatbar(Colors.green, (0.6 * 250 / 100)),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )));
   }
 }
 
 class BasicPanel extends StatefulWidget {
-
   final isBannerAdReady;
   final bannerAd;
 
